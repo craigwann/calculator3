13fbdcb6522959ff44aef74744af6c5de60875e7
craigwann <craigwann@me.com>
2022-04-04 12:36:53 -0700

    submit(function) and event.preventDefault()

--- js/scripts.js
+++ js/scripts.js
@@ -18,9 +18,11 @@ function divide(number1, number2) {
 
 // UI logic
 $(document).ready(function() {
-
-  const number1 = parseInt($("#add1").val());
-  const number2 = parseInt($("#add2").val());
-  
-  alert(add(number1, number2));
+  $("form#add").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    alert(result);
+  })
 });
