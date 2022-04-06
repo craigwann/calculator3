81012fc2b86e2d173dc244c36815f3be9c1e8573
craigwann <craigwann@me.com>
2022-04-06 14:48:16 -0700

    add UI logic form functions for subtract, mulitpy & divide

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ function multiply(number1, number2) {
 }
 
 function divide(number1, number2) {
-  return number1 * number2;
+  return number1 / number2;
 }
 
 // UI logic
@@ -24,5 +24,26 @@ $(document).ready(function() {
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
     $("#output").text(result);
-  })
+  });
+  $("form#sub").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#multiply1").val());
+    const number2 = parseInt($("#multiply2").val());
+    const result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#divide1").val());
+    const number2 = parseInt($("#divide2").val());
+    const result = divide(number1, number2);
+    $("#output").text(result);
+  });
 });
