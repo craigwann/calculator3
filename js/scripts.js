fd1d108b65d233f897d832ad7c688087fe37972e
craigwann <craigwann@me.com>
2022-04-15 14:19:51 -0700

    console.log(debugging)

--- js/scripts.js
+++ js/scripts.js
@@ -18,32 +18,13 @@ function divide(number1, number2) {
 
 // UI logic
 $(document).ready(function() {
-  $("form#add").submit(function(event){
+  $("form#calculator").submit(function(event){
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    console.log("1st number: ")
     const result = add(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#sub").submit(function(event){
-    event.preventDefault();
-    const number1 = parseInt($("#sub1").val());
-    const number2 = parseInt($("#sub2").val());
-    const result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#multiply").submit(function(event){
-    event.preventDefault();
-    const number1 = parseInt($("#multiply1").val());
-    const number2 = parseInt($("#multiply2").val());
-    const result = multiply(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#divide").submit(function(event){
-    event.preventDefault();
-    const number1 = parseInt($("#divide1").val());
-    const number2 = parseInt($("#divide2").val());
-    const result = divide(number1, number2);
     $("#output").text(result);
   });
 });
