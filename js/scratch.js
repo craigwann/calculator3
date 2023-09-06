787f78aaca7ffd22583d7447cfde61541c70647b
craigwann <craigwann@me.com>
2023-09-06 07:55:23 -0700

    add bootstrap card images

--- js/scratch.js
+++ js/scratch.js
@@ -1,3 +1,11 @@
 function click1() {
   document.getElementById("demo").innerHTML = "running script js/scratch.js - document.getElementById(`demo`).innerHTML";
 };
+
+function buttonFunction() {
+  alert("This project is in progress.")
+};
+
+function buttonFunction2() {
+  alert("21168044240299")
+};
